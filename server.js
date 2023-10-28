54d372cef5444e2607e1a77dedfaad0c79480b88
ilyasipek <ilyasipek1997@gmail.com>
2023-10-28 19:04:43 +0300

    Add a basic server with a sample get method

--- server.js
+++ server.js
@@ -1,1 +1,16 @@
-console.log("my first backend project");
+import express from 'express'
+import bodyParser from 'body-parser'
+
+const app = express()
+const PORT = 3000
+
+// Adds a middleware to parse every request/respose with json
+app.use(bodyParser.json())
+
+app.listen(PORT, () => {
+    return console.log(`Our server is working on http://localhost:${PORT}`)
+})
+
+app.get('/', (req, res) => {
+    res.status(200).send("Cool stuff")
+})
