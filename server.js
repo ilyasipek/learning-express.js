43d24d15548c1c03924b8078b660fb33f49bf509
ilyasipek <ilyasipek1997@gmail.com>
2023-10-28 19:51:57 +0300

    Add uuid package

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import bodyParser from 'body-parser'
+import usersRoute from './routes/users.js'
 
 const app = express()
 const PORT = 3000
@@ -7,10 +8,9 @@ const PORT = 3000
 // Adds a middleware to parse every request/respose with json
 app.use(bodyParser.json())
 
+// add routes
+app.use('/users', usersRoute)
+
 app.listen(PORT, () => {
     return console.log(`Our server is working on http://localhost:${PORT}`)
-})
-
-app.get('/', (req, res) => {
-    res.status(200).send("Cool stuff")
 })
