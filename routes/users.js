720ba5ad52e076397d2fc5dc751874a06b9cf08d
ilyasipek <ilyasipek1997@gmail.com>
2023-10-28 20:09:33 +0300

    Fix deleting all users issue

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,7 @@ import { v4 as uuidv4 } from 'uuid';
 
 const router = express.Router()
 
-const users = []
+let users = []
 
 router.get('/', (req, res) => {
     console.log(users)
@@ -31,10 +31,10 @@ router.get("/:id", (req, res) => {
 
 router.delete("/:id", (req, res) => {
     const { id } = req.params
-    const foundUser = users.find((user) => user.id === id)
-    users.pop(foundUser)
 
-    res.status(200).send("User is deleted successfully")
+    users = users.filter((user) => user.id !== id)
+
+    res.status(200).send(`User with ${id} id is deleted successfully`)
 })
 
 router.patch("/:id", (req, res) => {
